335578975f3f3ae73733addb56b67078f1f46e99
Bibhudatta Behera <143987793+bibhu-syenainfosoft@users.noreply.github.com>
2024-07-15 11:55:09 +0530

    ffrkj

--- Notes/3 SpringBoot_MVC/54/DevTools_With_JSP/src/main/java/com/example/demo/service/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.Employee;
-
-public interface EmployeeService {
-public Integer SaveEmployee(Employee ee);
-}
--- Notes/5 SpringBoot_RESTFUL-WEBSERVICES/83-88 MongoDB/84/SpringBootMongoDB/src/main/java/com/example/demo/repo/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.model.Book;
-
-public interface BookRepo extends MongoRepository<Book, String> {
-
-}
